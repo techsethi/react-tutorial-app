aa49249a577fb081a49964465853c733ad0b32a9
Pradeep Sethi <psethi@gmail.com>
2017-09-25 15:44:25 +0530

    changed name

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -20,7 +20,7 @@ var Navigation = () => {
       <div className="top-bar-right">
         <ul className="menu">
           <li className="menu-text">
-            Created by <a href="http://www.mead.io" target="_blank">Andrew Mead</a>
+            Created by <a href="http://siteindia.com" target="_blank">Pradeep Sethi</a>
           </li>
         </ul>
       </div>
